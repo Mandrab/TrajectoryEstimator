030616d8e20b3feb3375fe090019f38a3c34cc51
Paolo Baldini <paolobaldini01@gmail.com>
2020-11-08 15:52:05 +0100

    defined operations

--- main.m
+++ main.m
@@ -1,13 +1,30 @@
-function main(image_folder, ball_size)
+function main(images_folder, ball_size)
 
     % create a pattern to match images files in folder
-    images = dir(image_folder + "*.png");
+    images = dir(images_folder + "*.png");
+    
+    % create a data array
+    balls_data = [];
 
     for idx = 1:length(images)
-        image_name = image_folder + images(idx).name;
+        image_name = images_folder + images(idx).name;
         image = imread(image_name);
 
-        % find real world ball position
-        position = ball_position(image, ball_size);
+        % find ball position in image
+        balls_data(idx).image_position = ball_position(image, ball_size);
+        
+        % TODO calculate ball speed
+        %balls_data(idx).speed = ???
+        
+        % TODO get real world position
+        balls_data(idx).world_position = to_real_world(
+            balls_data(idx).image_position,
+            camera_model,
+            ball_size
+        );
+        
+        % TODO calculate trajectory
+        
+        % TODO plot result trajectory
     end
 end
