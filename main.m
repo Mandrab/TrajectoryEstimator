d3e21249ab94caba50da1262992c5f344e50bd89
Paolo Baldini <paolobaldini01@gmail.com>
2020-11-08 15:36:02 +0100

    added comments

--- main.m
+++ main.m
@@ -1,10 +1,13 @@
 function main(image_folder)
 
-    images = dir(image_folder + "*.png");           % create a pattern to match images files
+    % create a pattern to match images files in folder
+    images = dir(image_folder + "*.png");
+
     for idx = 1:length(images)
         image_name = image_folder + images(idx).name;
         image = imread(image_name);
-        
+
+        % find real world ball position
         position = ball_position(image);
     end
 end
