88df877dd935ffb470f0156265da12d79d4f2248
Paolo Baldini <paolobaldini01@gmail.com>
2020-11-09 23:16:35 +0100

    added function to convert from camera point to world point

--- to_real_world.m
+++ to_real_world.m
@@ -1,3 +1,10 @@
-function coordinate = to_real_world(position, camera_model, ball_size)
-    coordinate = 5
+function coordinate = to_real_world(position, radius, camera_model, ball_size)
+    disp(camera_model)
+    k = camera_model.Intrinsics.IntrinsicMatrix;
+    coordinate = k \ [position 1]';
+    
+    fx = camera_model.Intrinsics.FocalLength(1);
+    z = fx * radius / ball_size;
+    aa = coordinate(1:2);
+    coordinate = [coordinate(1:2)' z]';
 end
