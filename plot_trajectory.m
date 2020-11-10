93c006b5a4acd5e68ed18b031503026f46a34a69
Paolo Baldini <paolobaldini01@gmail.com>
2020-11-10 12:36:55 +0100

    simplified plot_trajectory

--- plot_trajectory.m
+++ plot_trajectory.m
@@ -1,8 +1,11 @@
-function plot_trajectory(image, balls)
-    positions = zeros(length(balls), 3);
-    for idx = 1:length(balls)
-        positions(idx,:) = [balls(idx).image_position 10];
+function plot_trajectory(image, points)
+    % create a vector containing x,y coordinate (on image) and circle size
+    positions = zeros(length(points), 3);
+    for idx = 1:length(points(:,1))
+        positions(idx,:) = [points(idx,:) 20];
     end
+    
+    % inserts all the circles 'marker' in the image in specified positions
     image = insertShape(image, 'FilledCircle', positions);
     imshow(image);
 end
