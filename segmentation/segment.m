f0c35657a99a17d145d01f4b37e10d7f3731c433
Paolo Baldini <paolobaldini01@gmail.com>
2020-11-10 22:21:43 +0100

    corrected commend and removed imshow

--- segmentation/segment.m
+++ segmentation/segment.m
@@ -3,14 +3,12 @@ function mask = segment(image)
 % Input:
 %   image: in which search the ball
 % Output:
-%   mask: of the ball or NaN if the ball has not been found TODO
+%   mask: of the ball
 
     % extract a dirt ball mask through thresholding
     raw_mask = binarize(image);
-    %imshow(image);
 
     % improve mask quality (more homogeneous, trying to remove noise)
     mask = homogenizes(raw_mask);
-    imshow(mask);
 
 end
