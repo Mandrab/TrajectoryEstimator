71ec6496c94ad42adda34997f8b1deaac1eaeb16
Paolo Baldini <paolobaldini01@gmail.com>
2020-11-10 23:17:36 +0100

    adjusted and commented main

--- main.m
+++ main.m
@@ -1,49 +1,64 @@
-function main(images_folder, ball_size, frame_time, camera_model)
+function camera_model = main(images_folder, trajectory_folder, ...
+        ball_size, camera_model, chessboard_square_size)
+% Given a calibration dataset (or a pre-computed camera_model) and a
+%   sequence of images predict the trajectory of the ball
+% Input:
+%   images_folder: main folder in which reside the datasets (calibration
+%       and trajectory). E.g.: ".", "./resources/images", ...
+%   trajectory_folder: folder in which trajectory images reside. E.g.:
+%       "trajectory"
+%   ball_size: size of the ball thrown
+%   camera_model: this parameter can be both a string both a pre-calculated
+%       camera model. In the first case, it represents the folder in which
+%       calibration images reside. E.g.: "calibration"
+%   chessboard_square_size: size of the checkboard square used in
+%       calibration. If the 'camera_model' is pre-calculated (the parameter
+%       is a string), this parameter can be NaN.
+% Output:
+%   camera_model: the camera_model used/calculated during the prediction
+%       (useful to repeat the test)
 
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Constants %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % if passed parameter is a string, use it as folder name for
+    % calibration images
+    if isstring(camera_model)
 
-    CALIBRATION_FOLDER = images_folder + "calibration_images/";
-    TRAJECTORY_FOLDER = images_folder + "trajectory_1/";
-
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Setup %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-    if not(isobject(camera_model))
-        % create a pattern to match images files in folder
-        images = dir(CALIBRATION_FOLDER + "*.png");
-        camera_model = calibration(images, 40);
+        % create a pattern to match images files in folder (calibration)
+        images = dir(images_folder + "/" + camera_model + "/" + "*.png");
+        camera_model = calibration(images, chessboard_square_size);
     end
 
-    %%%%%%%%%%%%%%%%%%%%%%%% Trajectory Estimation %%%%%%%%%%%%%%%%%%%%%%%%
-
-    % create a pattern to match images files in folder
-    images = dir(TRAJECTORY_FOLDER + "*.png");
+    % create a pattern to match images files in folder (trajectory)
+    images = dir(images_folder + "/" + trajectory_folder + "/" + "*.png");
 
     % create a data array
     balls_data = [];
 
+    % iterate every image in folder
     for idx = 1:length(images)
+
+        % load image
         image_name = images(idx).folder + "/" + images(idx).name;
         image = imread(image_name);
 
-        % find ball position in image
-        [balls_data(idx).image_position, balls_data(idx).image_radius] =...
-            ball_position(image);
-        
-        % calculate ball speed
-        %balls_data(idx).speed = 
-        
-        % get real world position
-        balls_data(idx).world_position = to_real_world(...
-            balls_data(idx).image_position, ...
-            balls_data(idx).image_radius, ...
-            camera_model, ...
-            ball_size ...
-        );
-        
-        % TODO calculate trajectory
-        trajectory = calculate_trajectory(balls_data);
-        
-        % TODO plot result trajectory
-        plot_trajectory(image, trajectory);
+        % search ball position and radius in image
+        [ball.image_position, ball.radius] = ball_data(image);
+
+        % make trajectory prediction if the ball is found
+        if not(isnan(ball.image_position))
+
+            % add ball data to set
+            ball.world_position = NaN;
+            balls_data = [balls_data ball];
+
+            % get real world position
+            balls_data(end).world_position = to_real_world(...
+                ball.image_position, ball.radius, camera_model, ball_size);
+
+            % calculate trajectory based on already loaded data
+            trajectory = calculate_trajectory(balls_data);
+
+            % plot calculated trajectory
+            plot_trajectory(image, trajectory);
+        end
     end
 end
